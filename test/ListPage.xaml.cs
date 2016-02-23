159bf195a3a3cf6ae3fcce3e8e2020462612c92a
Sudhir <san.thanki@gmail.com>
2016-02-23 19:45:45 +0530

    Initial commit

---
+++ test/ListPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace test
+{
+	public partial class ListPage : ContentPage
+	{
+		public ListPage ()
+		{
+			InitializeComponent ();
+			this.BindingContext = new CategoryViewModel ();
+		}
+	}
+}
+
---
+++ test/Models.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace test
+{
+	
+	public class Categories
+	{
+		public int Id{ get; set; }
+
+		public string Name { get; set; }
+
+		public List<CategoryItems> CategoryItem{ get; set; }
+	}
+
+	public class CategoryItems
+	{
+		public int Id { get; set; }
+
+		public string ItemName { get; set; }
+
+		public int CatId { get; set; }
+
+		public string ImageUrl { get; set; }
+	}
+}
+
